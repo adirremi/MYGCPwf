641026a41d386bad2d98ad799913168674b69ac7
adirremi <89388145+adirremi@users.noreply.github.com>
2024-08-16 03:37:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt-get update # מעדכן את רשימת החבילות של המערכת
 sudo apt-get install -y apache2 # מתקין את Apache
 sudo systemctl start apache2 # מפעיל את Apache
-sudo systemctl enable apache2 # מוודא שהשרת יתחיל אוטומטית עם הפעלת המערכת
+sudo systemctl enable apache2 # מוודא  שהשרת יתחיל אוטומטית עם הפעלת המערכת
 echo "<h1>Hey Amir /n It works!</h1>" | sudo tee /var/www/html/index.html # יוצר דף אינטרנט פשוט
 
